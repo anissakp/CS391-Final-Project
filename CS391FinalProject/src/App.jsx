9cd627d1d383fcf404bcb1ba1747927d4eb39ab3
mfmiller7 <mfmiller@bu.edu>
2024-04-10 17:03:08 -0400

    worked on button component, added global styling and worked on home page

--- CS391FinalProject/src/App.jsx
+++ CS391FinalProject/src/App.jsx
@@ -1,6 +1,26 @@
 import HomePage from "./pages/HomePage.jsx";
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+    * {
+        margin: 0;
+        box-sizing: border-box;
+    }
+    body {
+        margin: 0;
+        padding: 0;
+        font-family: fangsong, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        background-color: white;
+    }
+`;
+
 export default function App() {
   return (
-    <HomePage/>
+      <>
+        <GlobalStyle/>
+        <HomePage/>
+      </>
   )
 }
