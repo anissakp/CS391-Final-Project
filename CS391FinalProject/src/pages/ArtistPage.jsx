638d9280b54ad20279fe3e77e766f2ca1f37b31d
mfmiller7 <mfmiller@bu.edu>
2024-04-11 09:48:30 -0400

    working on artist page

--- CS391FinalProject/src/pages/ArtistPage.jsx
+++ CS391FinalProject/src/pages/ArtistPage.jsx
@@ -1,6 +1,41 @@
 // Margo
 // API: https://api.artic.edu/api/v1/artists/{id}
+import { useState, useEffect } from "react";
+import PropTypes from "prop-types";
 
-export default function ArtistPage(){
+export default function ArtistPage({ id }) {
+    const [artist, setArtist] = useState({});
 
+    useEffect(() => {
+        async function fetchData() {
+            try {
+                const rawData = await fetch(`https://api.artic.edu/api/v1/artists/${id}`);
+                const data = await rawData.json();
+
+                setArtist(data);
+            } catch (error) {
+                console.log("Something went wrong:", error);
+            }
+        }
+
+        fetchData();
+    }, [id]);
+
+    // fetching artist data...
+    if (!artist.title) {
+        return <div>Loading...</div>;
+    }
+
+    return (
+        <>
+            <h1>About {artist.title}</h1>
+            <h3>Birth date: {artist.birth_date}</h3>
+            <h3>Death date: {artist.death_date}</h3>
+            <p>{artist.description}</p>
+        </>
+    );
 }
+
+ArtistPage.propTypes = {
+    id: PropTypes.number.isRequired,
+};
