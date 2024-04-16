16a365b5ec68e12ad7a96cc462bd2061d2f94c91
Ana <anasof@bu.edu>
2024-04-16 12:04:09 -0400

    Artwork Page api call: app was modified for testing

---
+++ CS391FinalProject/src/pages/ArtworkPage.jsx
@@ -0,0 +1,41 @@
+import {useEffect,useState} from "react";
+import PropTypes from "prop-types";
+
+export default function ArtworkPage({ id }){
+    const [artwork,setArtwork]=useState([])
+    useEffect(()=>{
+        async function fetchData() {
+            try {
+                const raw = await fetch(`https://api.artic.edu/api/v1/artworks/${id}`);
+                const responseData = await raw.json();
+                console.log("Response from API:", responseData);
+
+                const { data } = responseData;
+                if (data) {
+                    setArtwork(data);
+                    console.log("Data fetched successfully:", data);
+                } else {
+                    console.log("No artwork data found");
+                }
+            } catch (error) {
+                console.error("Error fetching data:", error);
+            }
+        }
+
+        fetchData();
+    },[id]);
+    return (
+        <>
+            <h1>Artwork Page</h1>
+            <div>
+                    <h2>{artwork.title}</h2>
+                    <h3>{artwork.artist_title}</h3>
+                </div>
+
+        </>
+    )
+
+}
+ArtworkPage.propTypes = {
+    id: PropTypes.string.isRequired,
+};
