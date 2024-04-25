2a444a25e8940a4755b4d21412bfb896c5df55c6
mfmiller7 <mfmiller@bu.edu>
2024-04-24 23:45:27 -0400

    added artwork for each artist

--- CS391FinalProject/src/pages/ArtistPage.jsx
+++ CS391FinalProject/src/pages/ArtistPage.jsx
@@ -14,25 +14,39 @@ const StyledDiv=styled.div`
 // Styled component for the description of the artist
 const StyledDiv2=styled.div`
     margin: auto;
-    text-align: left; // want paragraph on the left
-    h3 {
-        text-align: center; // want header in the center
+    width: 60%;
+    @media screen and (max-width: 1200px) {
+        width: 75%;
     }
-    width: 70%; 
     @media screen and (max-width: 700px) {
         width: 90%;
     }
     padding: 0 1%;
-    border: #333 solid 2px;
     visibility: ${props => props.visibility}; // conditional visibility styling for whe there isn't an artist description
 `
 
+// Styled component for the artworks of this artist
+const StyledArtworks=styled.div`
+    display: grid;
+    grid-template-columns: repeat(2, 1fr); // two columns for smaller screens
+    @media screen and (min-width: 1250px) {
+        grid-template-columns: repeat(3, 1fr); // three columns when larger
+    }
+    gap: 20px;
+    padding: 0 4%;
+`
+
+// Styled component for each image
+const StyledImage=styled.img`
+    width: 100%; // same width for all images, 100% of the parent which is StyledArtworks
+`
+
 export default function ArtistPage() {
-    const [artist,setArtist]=useState([]) // stores artist data
     const { id } = useParams(); // extracts 'id' from the URL
 
+    const [artist,setArtist]=useState([]) // stores artist info
     useEffect(()=>{
-        async function fetchData() {
+        async function fetchInfo() {
             try {
                 const raw = await fetch(`https://api.artic.edu/api/v1/artists/${id}`);
                 const responseData = await raw.json();
@@ -48,31 +62,62 @@ export default function ArtistPage() {
                 console.error("Error fetching data:", error);
             }
         }
-        fetchData();
+        fetchInfo();
     },[id]); // so that data is re-fetched when passed a new id
+
+    const [artworks, setArtworks]=useState([]) // stores info of this artist's artworks
+    useEffect(()=>{
+        async function fetchArtworks() {
+            try {
+                const raw = await fetch(`https://api.artic.edu/api/v1/artworks/?artist_id=${id}`);
+                const responseData = await raw.json();
+                console.log("Response from API:", responseData);
+                const { data } = responseData;
+                if (data) {
+                    setArtworks(data);
+                    console.log("Data fetched successfully:", data);
+                } else {
+                    console.log("No artist data found");
+                }
+            } catch (error) {
+                console.error("Error fetching data:", error);
+            }
+        }
+        fetchArtworks();
+    },[id]); // so that data is re-fetched when passed a new id
+
     return (
         <StyledDiv>
             <h1>{artist.title}</h1>
             <div className="artist-info">
-                <h3>Birth date:</h3>
-                {artist.birth_date ? (
-                    <p>{artist.birth_date}</p> // Display birth date if available
+                {/*handling all possible cases of birth_date and death_date being null: */}
+                {artist.birth_date && artist.death_date ? (
+                    <h3>{artist.birth_date}-{artist.death_date}</h3>
+                ) : artist.birth_date ? (
+                    <p>{artist.title} was born in {artist.birth_date}</p>
+                ) : artist.death_date ? (
+                    <p>{artist.title} passed away in {artist.death_date}, no information is available on the year of their birth</p>
                 ) : (
-                    <p>{artist.title} birth date is unknown</p> // Fallback text if birth date is missing
+                    <p>There is no information on the birth or death years of {artist.title}</p>
                 )}
-                <h3>Death date: </h3>
-                {artist.death_date ? (
-                    <p>{artist.death_date}</p> // Display death date if available
-                ) : (
-                    <p>{artist.title} is still alive or death date is unknown</p> // Fallback text if death date is missing
-                )}
-                {/* Conditionally render this section only if a description exists */}
+                {/* Conditionally render this section only if a description exists: */}
                 <StyledDiv2 visibility={artist.description ? "visible" : "hidden"}>
-                    <h3>About:</h3>
                     {artist.description && (
-                        <div dangerouslySetInnerHTML={{ __html: artist.description }} /> // Since description is a string and that string has HTML in it, this takes the HTML formatting into account
+                        <div dangerouslySetInnerHTML={{__html: artist.description}}/> // Since description is a string and that string has HTML in it, this takes the HTML formatting into account
                     )}
                 </StyledDiv2>
+                <h2>Artwork by {artist.title}</h2>
+                <StyledArtworks>
+                    {artworks.map((artwork) => (
+                        <>
+                            <div key={artwork.id}>
+                                <StyledImage
+                                    src={`https://www.artic.edu/iiif/2/${artwork.image_id}/full/400,/0/default.jpg`}
+                                    alt={artwork.title}/>
+                            </div>
+                        </>
+                    ))}
+                </StyledArtworks>
             </div>
         </StyledDiv>
     );
