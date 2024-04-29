3e4b947494b5dfe346e0cba322d21e732a2ebbda
Anissa Patel <anissakp@bu.edu>
2024-04-29 01:30:33 -0400

    add comments to App file

--- CS391FinalProject/src/App.jsx
+++ CS391FinalProject/src/App.jsx
@@ -1,24 +1,34 @@
 // Anissa Patel: App.jsx
 import React from 'react';
 import { createBrowserRouter, Routes, Route, RouterProvider} from 'react-router-dom';
+import Header from "./components/Header.jsx"
 import HomePage from "./pages/HomePage.jsx";
 import ArtistPage from "./pages/ArtistPage.jsx";
 import ArtworkPage from "./pages/ArtworkPage.jsx";
-import Header from "./components/Header.jsx"
 
+// root component wraps the main routes and the header component
 function Root(){
   return(
     <>
-      <Header/>
+      {/* inserts the header component at the top of each page */}
+      <Header/>  
+
+      {/* container for route components where each Route defines a nav path */}
       <Routes>
+          {/* route for home page */}
+          {/* loads HomePage component when / is accessed */}
           <Route
             path='/'
             element={<HomePage/>}
           />
+          {/* route for artist page with ID */}
+          {/* loads ArtistPage component when /artist/:id is accessed */}
           <Route
             path='/artist/:id'
             element={<ArtistPage/>}
           />
+          {/* route for artwork page with ID */}
+          {/* loads ArtworkPage component when /artwork/:id is accessed */}
           <Route
             path='/artwork/:id'
             element={<ArtworkPage/>}
@@ -28,18 +38,21 @@ function Root(){
   );
 }
 
+// defines main router configuration using createBrowserRouter
 const router = createBrowserRouter([
   {
-    path: "/",  // Home page
+    // base path set to home route
+    path: "/", 
+    // root component is the main element for the base path
     element: <Root />,
     children: [
-      // { path: "/", element: <HomePage /> },
       { path: "artist/:id", element: <ArtistPage /> },
       { path: "artwork/:id", element: <ArtworkPage /> }
     ]
   }
 ]);
 
+// App component that is the entry point for the router
 export default function App(){
   return (
       <RouterProvider router={router} />
